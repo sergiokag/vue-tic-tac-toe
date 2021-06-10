2f770ac0b13cadec84240aa4736016c8578ede7c
Sergio Kagiema <sergio_kagiema@live.com>
2021-06-10 17:08:56 +0300

    Add store to vue app

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { emitter as $bus } from './emitter.js';
+import { store } from './models/store';
 
 const app = createApp(App);
 
 app.config.globalProperties.$bus = $bus;
+app.use(store);
 
 app.mount('#app');
