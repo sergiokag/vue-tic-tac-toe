b89b5202e76dafd2d58cb4563bb72bf301e4f7d5
Sergio Kagiema <sergio_kagiema@live.com>
2021-06-10 16:25:08 +0300

    Minor fix: remove app.use(Vuex)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
 
 import { emitter as $bus } from './emitter.js';
-import { store } from './models/store';
 
 const app = createApp(App);
 
 app.config.globalProperties.$bus = $bus;
-app.use(Vuex);
-
-console.log(store);
 
 app.mount('#app');
