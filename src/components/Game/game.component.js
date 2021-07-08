27ea4936fb7f919e9600f31c42aacb80464e7d03
Sergio Kagiema <sergio_kagiema@live.com>
2021-07-08 15:57:30 +0300

    First implementation with the  new vue syntax

--- src/components/Game/game.component.js
+++ src/components/Game/game.component.js
@@ -1,5 +1,5 @@
 // core
-import { onMounted, getCurrentInstance, toRefs } from 'vue';
+import { onMounted, getCurrentInstance, toRefs, computed } from 'vue';
 import { mapState } from 'redux-vuex';
 
 // components
@@ -34,66 +34,82 @@ export default {
             $bus.on("value-changed", (data) => onValueChange(data));
         });
 
+        const stateObj = mapState({
+            history: state => state.ticTacToe.history,
+            winner: state => state.ticTacToe.winner,
+            xIsNext: state => state.ticTacToe.xIsNext,
+            stepNumber: state => state.ticTacToe.stepNumber,
+            player1: state => state.players.player1,
+            player2: state => state.players.player2,
+        });
+
         // methods
         const onValueChange = ({ value, index }) => {
             GameModel.onPlay({ value, index });
         };
 
-        return {
-            ...toRefs(mapState({
-                history: state => state.ticTacToe.history,
-                winner: state => state.ticTacToe.winner,
-                xIsNext: state => state.ticTacToe.xIsNext,
-                stepNumber: state => state.ticTacToe.stepNumber,
-                player1: state => state.players.player1,
-                player2: state => state.players.player2,
-            })),
-            onValueChange,
+        const onGameRestart = () => {
+            GameModel.reset();
         };
-    },
-    computed: {
-        squares() {
-            const history = this.history.slice(0, this.stepNumber + 1);
+
+        const onSelectedMove = (step) => {
+            GameModel.selectHistoryStep(step);
+        };
+
+        const onUpdateValue = ({ playerId, value }) => {
+            GameModel.setPlayerName({ playerId, value });
+        };
+
+        const onGameSave = () => {
+            GameModel.saveGame();
+        };
+
+        // computed
+        const squares = computed(() => {
+            const history = stateObj.history.slice(0, stateObj.stepNumber + 1);
             const current = history[history.length - 1];
             const squares = current.squares.slice();
             return squares;
-        },
-        isFullList() {
-            return this.squares.filter((square) => !!square).length === 9;
-        },
-        message() {
-            if (!this.winner && this.isFullList) {
+        });
+
+        const isFullList = computed(() => {
+            return squares.value.filter((square) => !!square).length === 9;
+        });
+
+        const message = computed(() => {
+            if (!stateObj.winner && isFullList.value) {
                 return "Draw";
             }
-            if (!this.winner) {
-                return `Next player: ${this.xIsNext ? this.player1 || "Player 1" : this.player2 || "Player 2"
+            if (!stateObj.winner) {
+                return `Next player: ${stateObj.xIsNext ? stateObj.player1 || "Player 1" : stateObj.player2 || "Player 2"
                     }`;
             }
-            return `Winner: ${this.playerName}`;
-        },
-        playerName() {
-            return this.winner && this.winner === "X"
-                ? this.player1 || "Player 1"
-                : this.winner === "O"
-                    ? this.player2 || "Player 2"
+            return `Winner: ${playerName.value}`;
+        });
+
+        const playerName = computed(() => {
+            return stateObj.winner && stateObj.winner === "X"
+                ? stateObj.player1 || "Player 1"
+                : stateObj.winner === "O"
+                    ? stateObj.player2 || "Player 2"
                     : "";
-        },
-        isBtnDisabled() {
-            return (this.message !== 'Draw' && !this.winner) || false;
-        }
-    },
-    methods: {
-        onGameRestart() {
-            GameModel.reset();
-        },
-        onSelectedMove(step) {
-            GameModel.selectHistoryStep(step);
-        },
-        onUpdateValue({ playerId, value }) {
-            GameModel.setPlayerName({ playerId, value });
-        },
-        onGameSave() {
-            GameModel.saveGame();
-        },
-    },
+        });
+
+        const isBtnDisabled = computed(() => {
+            return (message.value !== 'Draw' && !stateObj.winner) || false;
+        });
+
+        return {
+            ...toRefs(stateObj),
+            squares,
+            message,
+            playerName,
+            isBtnDisabled,
+            onValueChange,
+            onGameSave,
+            onUpdateValue,
+            onSelectedMove,
+            onGameRestart,
+        };
+    }
 };
